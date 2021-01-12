5fc6eb64851fb07a977151d1e42f7d515466e5e7
runtime0x00 <mohamedelkady484@gmail.com>
2021-01-12 17:29:13 +0200

    first version of my project

---
+++ scripts/json.js
@@ -0,0 +1,16 @@
+// JSON Document
+data = {"quizcontent": [
+        {"question": "What is the full name of Mark Zuckerberg?", "image": "1.jpg", "correct": 4, "a1":"Mark", "a2":"Fritz","a3":"Mark Elliot","a4":"Mark Elliot Zuckerberg"},
+    {"question": "Mark Zuckerberg is best-known for co-founding which popular social networking site?", "image": "2.jpg", "correct": 1, "a1":"Facebook", "a2":"Twitter","a3":"Tumblr","a4":"Chaino"},
+	{"question": "Why is Facebook’s colour scheme blue?", "image": "3.jpg", "correct": 4, "a1":"people choice", "a2":"Mark's wife Choice","a3":"Randomly Chosen","a4":"Zuckerberg is color blind"},
+	{"question": "The spouse of Mark Zuckerberg is:", "image": "4.jpg", "correct": 1, "a1":"Priscilla Chan", "a2":"Audrey Hepburn","a3":"Meryl Streep","a4":"Michelle Pfeiffer"},
+	{"question": "Where was Mark Zuckerberg born?", "image": "4.jpg", "correct": 1, "a1":"United States", "a2":"England","a3":"France","a4":"Germany"},
+	{"question": "A movie based on Mark Zuckerberg and the founding years of Facebook, ‘The Social Network’, was released on:", "image": "4.jpg", "correct": 1, "a1":"October 1, 2010", "a2":"August 1, 2010","a3":"December 1, 2010","a4":"Januray 1, 2010"},
+	{"question": "The hometown of Mark Zuckerberg is:", "image": "4.jpg", "correct": 1, "a1":"Dobbs Ferry, New York, U.S", "a2":"Dobbs Ferry, California, U.S","a3":"Dobbs Ferry, Georgia, U.S","a4":"Dobbs Ferry, pennsylvania, U.S"},
+	{"question": "Zuckerberg launched Facebook from his Harvard dormitory room on:", "image": "4.jpg", "correct": 2, "a1":"May 4, 2004", "a2":"February 4, 2004","a3":"Januray 4, 2004","a4":"February 25, 2004"},
+    {"question": "On February 4, 2004, he launched Facebook which name?", "image": "4.jpg", "correct": 2, "a1":"facebook.com", "a2":"thefacebook.com","a3":"thefacebook.com","a4":"thebook.com"},
+    {"question": "Facebook is an online social networking service headquartered in:", "image": "4.jpg", "correct": 1, "a1":"California", "a2":"New York","a3":"Georgia","a4":"pennsylvania"},
+    ],
+	
+};
+
